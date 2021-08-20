5781d731a69bd710b44751af2296a24e2eeed8a4
avalladaresm <avalladaresm@unitec.edu>
2021-08-20 17:06:55 -0600

    added initial config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { useEffect } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import NProgress from "nprogress";
+import "tailwindcss/tailwind.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      cacheTime: Infinity,
+      staleTime: 300000,
+      retry: false
+    }
+  }
+});
+
+function AndromedaApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    NProgress.configure({ showSpinner: true });
+    let routeChangeStart = () => NProgress.start();
+    let routeChangeComplete = () => NProgress.done();
+
+    router.events.on("routeChangeStart", routeChangeStart);
+    router.events.on("routeChangeComplete", routeChangeComplete);
+    router.events.on("routeChangeError", routeChangeComplete);
+    return () => {
+      router.events.off("routeChangeStart", routeChangeStart);
+      router.events.off("routeChangeComplete", routeChangeComplete);
+      router.events.off("routeChangeError", routeChangeComplete);
+    };
+  }, []);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>andromedaapp</title>
+      </Head>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
-export default MyApp
+export default AndromedaApp;
